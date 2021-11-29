e27e55dc6768bf7e0553c3a32e86502499dc09ce
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2021-11-29 11:39:57 -0500

    MySQL Demo

---
+++ mysqldemo/src/test/java/com/leecottrell/mysqldemo/MysqldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leecottrell.mysqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
